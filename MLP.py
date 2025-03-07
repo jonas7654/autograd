323ffdb203cb1d0f0870fae1435b00e2a991e3a8
Jonas Veit <me322@uni-heidelberg.de>
2025-03-07 16:32:23 +0100

    added very simple MLP for educational purposes

--- MLP.py
+++ MLP.py
@@ -10,8 +10,17 @@ class Neuron:
     def __call__(self, x : Value):
         # w * x + b : w * x is a dot product
         Z = sum((w * x for w, x in zip(self.weights, x)) , self.bias)
-        A = Z.tanh()
+        A = Z.sigmoid()
         return A
+    
+    def update(self, lr):
+        for w in self.weights:
+            w.value -= lr * w.gradient
+            
+    def zero_grad(self):
+        for w in self.weights:
+            w.zero_grad()
+        self.bias.zero_grad()
         
 
 class Layer:
@@ -20,8 +29,69 @@ class Layer:
     
     def __call__(self, x):
         return [neuron(x) for neuron in self.neurons]
-        
-        
     
+    def update(self, lr):
+        for neuron in self.neurons:
+            neuron.update(lr)
+            
+    def zero_grad(self):
+        for neuron in self.neurons:
+            neuron.zero_grad()
+        
     def __repr__(self):
         return f"Number of neurons: {len(self.neurons)}"
+    
+    
+class MLP:
+    def __init__(self, input_size : int, layer_sizes : list):
+        l = [input_size] + layer_sizes
+        self.layers = []
+        for i in range(len(l) - 1):
+            self.layers.append(Layer(l[i], l[i + 1]))
+            
+    
+    def forward(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+
+    def update(self, lr):
+        for layer in self.layers:
+            layer.update(lr)
+    
+    def zero_grad(self):
+        for layer in self.layers:
+            layer.zero_grad()
+    
+    # I assume that the input data fits into memory so no batch size
+    def train(self, x, epochs = None, loss_function = None, lr = 0.001):
+        if (loss_function == None or not isinstance(loss_function, callable)):
+            def loss(y_pred, y_true):
+                inner = [(y1 - y2) ** 2 for y1, y2 in zip(y_pred, y_true)]
+                return sum(inner) / len(x)            
+            loss_function = loss
+
+        
+        if not isinstance(x, list):
+               x = [x]  # Convert single Value to a list
+        if not isinstance(x, Value):
+            x = [Value(xvalue) for xvalue in x]
+            
+        
+        for epoch in range(epochs):
+           output = self.forward(x)
+ 
+           
+           loss = loss_function(output, x)   
+           loss.backward()
+           
+           self.update(lr)
+           self.zero_grad()
+           
+           if epoch % 5 == 0:
+               print(f"Epoch: {epoch} \n Loss: {loss}")
+           
+           
+    
+    def __repr__(self):
+        return f"Total Layers: {len(self.layers)}"
--- autograd.py
+++ autograd.py
@@ -25,6 +25,11 @@ class Value:
         
         return result
     
+    def __radd__(self, other):
+        """Handles int/float + Value (calls __add__)"""
+        return self.__add__(other)  # This makes 5 + Value(3) work!
+
+    
     def __mul__(self, other):
         if not isinstance(other, Value):
             other = Value(other)
@@ -40,6 +45,69 @@ class Value:
         result._backward = backward
         
         return result
+
+    def __rmul__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+        return other.__mul__(self)
+    
+    def __pow__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+            
+        result = Value(self.value ** other.value)
+        result.leftChild = self
+        result.rightChild = other
+        result.operator = "pow"
+        
+        def backward():
+            self.gradient += result.gradient * (other.value * (self.value **(other.value - 1)))
+            if self.value <= 0:
+                self.value = 1e-10
+            other.gradient += result.gradient * ((self.value ** other.value) * math.log(self.value))
+        result._backward = backward
+        
+        return result
+    
+    def __sub__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+            
+        result = Value(self.value - other.value)
+        result.leftChild = self
+        result.rightChild = other
+        result.operator = "-"
+        
+        def backward():
+            self.gradient += result.gradient * 1.0
+            other.gradient += result.gradient * (-1.0)
+        result._backward = backward
+        
+        return result
+    
+    def __truediv__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+        result = Value(self.value / other.value)
+        result.leftChild = self
+        result.rightChild = other
+        result.operator = "/"
+        
+        def backward():
+            self.gradient += result.gradient * (1 / other.value)  # ∂(a/b) / ∂a = 1/b
+            other.gradient += result.gradient * (-(self.value) / (other.value ** 2))  # ∂(a/b) / ∂b = -a / b²
+        result._backward = backward
+        
+        return result
+    
+    def __rtruediv__(self, other):
+        if not isinstance(other, Value):
+            other = Value(other)
+        return other.__truediv__(self)
+    
+    # Define unary minus
+    def __neg__(self):
+        return Value(-self.value)
     
     def exp(self):
         result = Value(math.exp(self.value))
@@ -76,9 +144,36 @@ class Value:
         result._backward = backward
         
         return result
-            
+    
+    def sigmoid(self):
+        result = Value(math.exp(self.value) / (1 + math.exp(self.value)))
+        result.leftChild = self
+        result.rightChild = None
+        result.operator = "sigmoid"
+        def backward():
+            derivative_sigmoid = result.value * (1 - result.value)
+            self.gradient += result.gradient * derivative_sigmoid
+        result._backward = backward
+        
+        return result
+                
     def backward(self):
+        """
+        We want to begin at the current Node so we set the gradient of it to:
+        df/df = 1.0
+
+        Furthermore we need to apply a topological ordering to the computational graph in order to
+        deal with things like this:
+        
+        node1 = a + b
+        node2 = node1 * c
+        node3 = node2 * a
+        
+        where a has an edge to node1 but also to node 2
+        """
         self.gradient = 1.0
+        
+        
         nodes_visited = set() # set's are implemented as hash tables which have expected lookup time of O(1)
         nodes_topo = []
         def topological_sort(node):
@@ -105,4 +200,4 @@ class Value:
         
     
     def __repr__(self):
-        return f"Node value: {self.value} \nNode gradient: {self.gradient} \n"
+        return f"Value({self.value})"
